1e934404488d48ca2f7e67f3aa5147bc7a17a360
Oliver Ehrenmüller <oliver.ehrenmueller@gmail.com>
2020-03-05 08:49:23 +0100

    Created Flutter-Issue-Demo

---
+++ ComboBoxFlutterIssue/App.xaml.cs
@@ -0,0 +1,7 @@
+﻿namespace ComboBoxFlutterIssue
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App { }
+}
---
+++ ComboBoxFlutterIssue/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ComboBoxFlutterIssue
+{
+    public enum SortBy
+    {
+        Opened,
+        Modified,
+        Name
+    }
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public SortBy SortBy { get; set; } = SortBy.Name;
+
+        public static Tuple<SortBy, string>[] SortByOptions { get; } =
+        {
+            new Tuple<SortBy, string>(SortBy.Opened, "Kürzlich geöffnet"),
+            new Tuple<SortBy, string>(SortBy.Modified, "Kürzlich geändert"),
+            new Tuple<SortBy, string>(SortBy.Name, "Name")
+        };
+    }
+}
